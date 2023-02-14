aab082b294322501a7457b8c213d91e81a15f4ac
Jonah <47046556+jonahb55@users.noreply.github.com>
2023-02-14 17:34:35 -0500

    Fix formatting

--- src/main/java/org/littletonrobotics/frc2023/commands/ChargeStationAutoBalance.java
+++ src/main/java/org/littletonrobotics/frc2023/commands/ChargeStationAutoBalance.java
@@ -1,19 +1,20 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// Copyright (c) 2023 FRC 6328
+// http://github.com/Mechanical-Advantage
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
 
 package org.littletonrobotics.frc2023.commands;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import org.littletonrobotics.frc2023.Constants;
 import org.littletonrobotics.frc2023.FieldConstants;
 import org.littletonrobotics.frc2023.subsystems.drive.Drive;
 import org.littletonrobotics.frc2023.util.LoggedTunableNumber;
-
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class ChargeStationAutoBalance extends CommandBase {
   private final Drive drive;
@@ -21,12 +22,16 @@ public class ChargeStationAutoBalance extends CommandBase {
   private Rotation2d drivePitch;
   private Rotation2d driveRoll;
 
-  private static final LoggedTunableNumber AutoBalanceKP = new LoggedTunableNumber("ChargeStationAutoBalance/AutoBalanceKP");
-  private static final LoggedTunableNumber AutoBalanceKI = new LoggedTunableNumber("ChargeStationAutoBalance/AutoBalanceKI");
-  private static final LoggedTunableNumber AutoBalanceKD = new LoggedTunableNumber("ChargeStationAutoBalance/AutoBalanceKD");
+  private static final LoggedTunableNumber AutoBalanceKP =
+      new LoggedTunableNumber("ChargeStationAutoBalance/AutoBalanceKP");
+  private static final LoggedTunableNumber AutoBalanceKI =
+      new LoggedTunableNumber("ChargeStationAutoBalance/AutoBalanceKI");
+  private static final LoggedTunableNumber AutoBalanceKD =
+      new LoggedTunableNumber("ChargeStationAutoBalance/AutoBalanceKD");
   double driveSpeedMetersPerSec = 5.0;
 
-  private final PIDController pidController = new PIDController(AutoBalanceKP.get(), AutoBalanceKI.get(), AutoBalanceKD.get());
+  private final PIDController pidController =
+      new PIDController(AutoBalanceKP.get(), AutoBalanceKI.get(), AutoBalanceKD.get());
 
   /** Creates a new ChargeStationAutoBalance. */
   public ChargeStationAutoBalance(Drive drive) {
@@ -35,17 +40,17 @@ public class ChargeStationAutoBalance extends CommandBase {
   }
 
   static {
-  switch (Constants.getRobot()) {
-    case ROBOT_2023P:
-    case ROBOT_SIMBOT:
-      AutoBalanceKP.initDefault(1.0);
-      AutoBalanceKI.initDefault(0.0);
-      AutoBalanceKD.initDefault(1.0);
-      break;
-    default:
-      break;
+    switch (Constants.getRobot()) {
+      case ROBOT_2023P:
+      case ROBOT_SIMBOT:
+        AutoBalanceKP.initDefault(1.0);
+        AutoBalanceKI.initDefault(0.0);
+        AutoBalanceKD.initDefault(1.0);
+        break;
+      default:
+        break;
+    }
   }
-}
 
   // Called when the command is initially scheduled.
   @Override
@@ -57,8 +62,7 @@ public class ChargeStationAutoBalance extends CommandBase {
     // update tunnable numbers if changed
     if (AutoBalanceKD.hasChanged(hashCode())
         || AutoBalanceKI.hasChanged(hashCode())
-        || AutoBalanceKP.hasChanged(hashCode()))
-    {
+        || AutoBalanceKP.hasChanged(hashCode())) {
       pidController.setP(AutoBalanceKP.get());
       pidController.setI(AutoBalanceKI.get());
       pidController.setD(AutoBalanceKD.get());
@@ -69,20 +73,21 @@ public class ChargeStationAutoBalance extends CommandBase {
     drivePitch = drive.getPitchRotation();
     driveYaw = drive.getRotation();
 
-    double chargeStationAngle = driveRoll.getRadians() * driveYaw.getSin() + drivePitch.getRadians() * driveYaw.getCos();
+    double chargeStationAngle =
+        driveRoll.getRadians() * driveYaw.getSin() + drivePitch.getRadians() * driveYaw.getCos();
 
     // P controller
     double driveSpeedAfterPID = pidController.calculate(chargeStationAngle);
 
     // check boundaries for left of charge station
-    if (drive.getPose().getX() <= FieldConstants.Community.chargingStationLeftY && driveSpeedAfterPID < 0)
-    {
+    if (drive.getPose().getX() <= FieldConstants.Community.chargingStationLeftY
+        && driveSpeedAfterPID < 0) {
       driveSpeedAfterPID = 0;
     }
 
     // check boundaries for right of charge station
-    if (drive.getPose().getX() <= FieldConstants.Community.chargingStationRightY && driveSpeedAfterPID > 0)
-    {
+    if (drive.getPose().getX() <= FieldConstants.Community.chargingStationRightY
+        && driveSpeedAfterPID > 0) {
       driveSpeedAfterPID = 0;
     }
 
